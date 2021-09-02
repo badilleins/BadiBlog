b33fa3d72767583edba46b8ad74e2fb7e906a80e
badilleins <badilleins@gmail.com>
2021-09-02 18:37:48 -0500

    Acabando el blog

---
+++ javascript/user-actions.js
@@ -0,0 +1,20 @@
+document.getElementById("contact")
+// let number = "0997831974"
+function copyNumber() {
+  // Crea un campo de texto "oculto"
+  let aux = document.createElement("input");
+  // Asigna el contenido del elemento especificado al valor del campo
+  aux.setAttribute("value", "0997831974");
+  // Añade el campo a la página
+  document.body.appendChild(aux);
+  // Selecciona el contenido del campo
+  aux.select();
+  // Copia el texto seleccionado
+  document.execCommand("copy", swal("Mi número ha sido copiado al portapapeles", "Contactame vía whatsapp "));
+  // Elimina el campo de la página
+  document.body.removeChild(aux);
+}
+
+function mandarCorreo(){
+  window.open('mailto:badilleins@gmail.com');
+}
